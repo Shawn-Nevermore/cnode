dbd5cec7c43fc73480e05db9e0775c407f41602d
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-09-11 23:32:27 +0800

    添加SideBar组件，隶属于Main

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,13 +1,18 @@
 <template>
-    $END$
+  <div class="sidebar"></div>
 </template>
 
 <script>
-    export default {
-        name: "SideBar"
-    }
+export default {
+  name: "SideBar"
+};
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+.sidebar
+    width: 290px
+    font-size: 14px
+    margin: 0 0 20px 15px
+    height: 100vh
+    background: green
 </style>

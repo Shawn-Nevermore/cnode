6a38e6ddb99041aee8829d20dc056875eb3dc488
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-09-11 23:31:00 +0800

    添加Home/Main组件

---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main">
+    <PostList></PostList>
+    <SideBar></SideBar>
+  </div>
+</template>
+
+<script>
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+
+export default {
+  name: "Main",
+  components: {
+    PostList,
+    SideBar
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.main
+    width: 100%
+    max-width: 1400px
+    min-width: 960px
+    margin: 65px auto
+    min-height: 400px
+    display: flex
+</style>

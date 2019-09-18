67dbffa88c3a4409a42fc7ae7b90d71123e71f95
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-09-19 00:04:52 +0800

    改为Float布局

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -5,42 +5,40 @@
       <img src="" alt="" />
     </div>
     <div class="panel">
+      <div class="toolbar">
+        <span class="topic-tab current-tab">全部</span>
+        <span class="topic-tab">精华</span>
+        <span class="topic-tab">分享</span>
+        <span class="topic-tab">问答</span>
+        <span class="topic-tab">招聘</span>
+      </div>
       <ul class="topic_list">
-        <li class="header">
-          <div class="toolbar">
-            <span>全部</span>
-            <span>精华</span>
-            <span>分享</span>
-            <span>问答</span>
-            <span>招聘</span>
-          </div>
-        </li>
-        <li class="cell" v-for="post in posts" :key="post.id">
-          <div class="leftPart">
-            <!--头像-->
-            <a class="avatar" href="#"><img :src="post.author.avatar_url" alt="头像"/></a>
+        <li class="cell clearfix" v-for="post in posts" :key="post.id">
+          <!--头像-->
+          <a class="avatar" href="#">
+              <img :src="post.author.avatar_url" alt="avatar"/>
+          </a>
 
-            <!--回复数/点击数-->
-            <span class="reply_visit_count">
-              <span class="reply_count">{{ post.reply_count }}</span>
-              <span class="count_seperator">/</span>
-              <span class="visit_count">{{ post.visit_count }}</span>
-            </span>
+          <!--回复数/点击数-->
+          <span class="reply_visit_count">
+            <span class="reply_count">{{ post.reply_count }}</span>
+            <span class="count_seperator">/</span>
+            <span class="visit_count">{{ post.visit_count }}</span>
+          </span>
+
+          <!--最后更新时间 -->
+          <div class="last_time">
+            <!--              <button @click="getAvatar(post.id)" :key="post.id">GET</button>-->
+            <img :src="reply_avatar" alt="" />
+            <span class="last_active_time">{{
+              post.last_reply_at | formatDate
+            }}</span>
           </div>
 
+          <!--标题-->
+          <div class="topic_title_wrapper">
             <span class="topic_list_tab">置顶</span>
-            <!--标题-->
-            <div class="topic_title_wrapper">
-              <a class="topic_title" :href="post.title">{{ post.title }}</a>
-            </div>
-
-          <div class="rightPart">
-            <!--最后更新时间 -->
-            <span class="last_time">
-<!--              <button @click="getAvatar(post.id)" :key="post.id">GET</button>-->
-              <img :src="reply_avatar" alt=""/>
-              <span class="last_active_time">{{ post.last_reply_at | formatDate }}</span>
-            </span>
+            <a class="topic_title" :href="post.title">{{ post.title }}</a>
           </div>
         </li>
       </ul>
@@ -100,86 +98,90 @@ export default {
 
 <style lang="stylus" scoped>
 .postlist
-    background: #fff
     max-width: 1095px
     .panel
         a
             text-decoration none
             color: #778087
+        .toolbar
+            padding: 10px
+            background-color: #f6f6f6
+            border-radius: 5px 5px 0 0
+            .topic-tab.current-tab
+                background-color: #80bd01
+                color: #fff
+                padding: 3px 4px
+                border-radius: 3px
+            .topic-tab
+                margin: 0 10px
+                color: #80bd01
         .topic_list
             list-style: none
-            padding 0   /*清除默认Padding*/
             .cell
+                position: relative
                 &:hover
                     background: #f5f5f5
                 &:first-child
                     border-top 0
-                display: flex
-                justify-content: space-between
-                align-items: center
+                background: #fff
                 border-top 1px solid #f5f5f5
                 padding: 10px
-                .leftPart
-                    width: 100px
-                    display: flex
-                    align-items: center
-                    .avatar
-                        display: flow-root
-                        img
-                            width: 30px
-                            border-radius: 3px
-                    .reply_visit_count
-                        width: 70px
-                        text-align center
-                        .reply_count
-                            color: #9e78c0
-                        .count_seperator
-                            font-size 10px
-                        .visit_count
-                            font-size: 10px
-                            color: #b4b4b4
+                .avatar
+                    float: left
+                    img
+                        width: 30px
+                        height: 30px
+                        border-radius: 3px
+                .reply_visit_count
+                    float: left
+                    width: 70px
+                    text-align center
+                    line-height: 30px
+                    height: 30px
+                    .reply_count
+                        color: #9e78c0
+                    .count_seperator
+                        font-size 10px
+                    .visit_count
+                        font-size: 10px
+                        color: #b4b4b4
 
-                .topic_list_tab
-                  background: #e5e5e5
-                  color: #999
-                  padding: 2px 4px
-                  margin: 0 5px
-                  border-radius: 3px
-                  font-size: 12px
 
+                .last_time
+                    float: right
+                    margin-left: 50px
+                    img
+                        width: 20px
+                        background: #000
+                    .last_active_time
+                        display: inline-block
+                        min-width: 50px
+                        font-size 11px
+                        color #778087
+                        text-align: right
+                        white-space: nowrap
                 .topic_title_wrapper
-                    display: flow-root
-                    flex: 1
                     white-space: nowrap
-                    max-width: 752.5px
+                    .topic_list_tab
+                        background: #e5e5e5
+                        line-height: 30px
+                        height: 30px
+                        color: #999
+                        padding: 2px 4px
+                        /*margin: 0 5px*/
+                        border-radius: 3px
+                        font-size: 12px
                     .topic_title
                         &:hover
                             text-decoration underline
                         &:visited
                             color: #888
+                        display: inline-block
+                        line-height: 30px
                         color: #333
                         width: 70%
                         text-overflow ellipsis
                         white-space: nowrap
                         font-size: 16px
-                        display: block
                         overflow: hidden
-
-                .rightPart
-                    flex: none
-                    vertical-align middle
-
-                    .last_time
-                        display: flow-root
-                        min-width: 70px
-                        img
-                            width: 20px
-                            background: #000
-                        .last_active_time
-                            display: inline-block
-                            min-width: 50px
-                            font-size 11px
-                            color #778087
-                            text-align: right
-                            white-space: nowrap
 </style>
